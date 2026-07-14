da79bef74e350c018abfe161de0ec9771515d74b
Hüda Nur Sel <iii.hudanursel@gmail.com>
2026-07-15 02:48:01 +0300

    Add files via upload

---
+++ 20while_ile_geriye_sayma.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/* run this program using the console pauser or add your own getch, system("pause") or input loop */
+
+int main(int argc, char *argv[]) {
+	
+	
+	//10 dan baslayýp sayýlarý 1 azalta azalta 1 e kadar olan tum sayýlarý ekrana bastýrýnýz while dongusuyle
+	int i=10;
+	while(i>=1){
+		printf("%d ",i);
+		i=i-1;
+	}
+	
+	
+	
+	return 0;
+}
